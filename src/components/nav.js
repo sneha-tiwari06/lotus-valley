92aad690ec18be48834471e83b5c4af4ee2944d4
sneha-tiwari06 <tiwarisneha491@gmail.com>
2024-06-13 15:51:37 +0530

    Ui updated

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,7 +1,15 @@
 import React from 'react'
-
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/bundle';
+import 'swiper/css/autoplay';
+import { Autoplay } from 'swiper/modules';
+import './button.css'
 function Nav() {
     return (
+
         <div className='w-100'>
             <nav className="navbar2 navbar-expand-lg ">
                 <div className='container'>
@@ -15,7 +23,7 @@ function Nav() {
                             <li className='items'><a href='/'>Careers</a></li>
                             <li className='items'><a href='/'>School Calendar</a></li>
                             <li className='items'><a href='/'>Alumini</a></li>
-                            <li className='items'><a href='/'>eMagezine</a></li>
+                            <li className='items'><a href='/'>eMagazine</a></li>
                             <li className='items'><a href='/'>CUET</a></li>
                         </ul>
 
@@ -24,9 +32,94 @@ function Nav() {
 
 
             </nav>
-            <div className='banner'>
-                <img className ='banner-img'src='../assets/banner.jpg' alt=''></img>
-            </div>
+            <Swiper
+                spaceBetween={50}
+                slidesPerView={1}
+                autoplay={{
+                    delay: 5000,
+                    disableOnInteraction: false,
+                }}
+                modules={[Autoplay]}
+                pagination={{ clickable: true }}
+
+            >
+
+                <SwiperSlide>
+                    <div className='banner'>
+                        <img className='banner-img' src='../assets/benner (1).jpg' alt='Banner' />
+                        <div className='overlay'>
+                          
+                            <button class="button-85-1" >Virtual Tour</button>
+                            <button class="button-85-1">Career</button>
+                            
+                        </div>
+                        <div className='overlay-bottom-text'>
+                            <h2 className='banner-text'>Making a Difference to Education so that Education can make a Difference to the World.</h2>
+                        <p className='banner-subtext'>Arise! Awake! and Stop not till the goal is reached</p>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className='banner'>
+                        <img className='banner-img' src='../assets/benner (2).jpg' alt='Banner' />
+                        <div className='overlay'>
+                          
+                            <button class="button-85-1" >Virtual Tour</button>
+                            <button class="button-85-1">Career</button>
+                            
+                        </div>
+                        <div className='overlay-bottom-text'>
+                            <h2 className='banner-text'>Making a Difference to Education so that Education can make a Difference to the World.</h2>
+                        <p className='banner-subtext'>Arise! Awake! and Stop not till the goal is reached</p>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className='banner'>
+                        <img className='banner-img' src='../assets/benner (3).jpg' alt='Banner' />
+                        <div className='overlay'>
+                          
+                            <button class="button-85-1" >Virtual Tour</button>
+                            <button class="button-85-1">Career</button>
+                           
+                        </div>
+                        <div className='overlay-bottom-text'>
+                            <h2 className='banner-text'>Making a Difference to Education so that Education can make a Difference to the World.</h2>
+                        <p className='banner-subtext'>Arise! Awake! and Stop not till the goal is reached</p>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className='banner'>
+                        <img className='banner-img' src='../assets/benner (4).jpg' alt='Banner' />
+                        <div className='overlay'>
+                          
+                            <button class="button-85-1" >Virtual Tour</button>
+                            <button class="button-85-1">Career</button>
+                            
+                        </div>
+                        <div className='overlay-bottom-text'>
+                            <h2 className='banner-text'>Making a Difference to Education so that Education can make a Difference to the World.</h2>
+                        <p className='banner-subtext'>Arise! Awake! and Stop not till the goal is reached</p>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className='banner'>
+                        <img className='banner-img' src='../assets/banner4.jpg' alt='Banner' />
+                        <div className='overlay'>
+                          
+                            <button class="button-85-1" >Virtual Tour</button>
+                            <button class="button-85-1">Career</button>
+                            
+                        </div>
+                        <div className='overlay-bottom-text'>
+                            <h2 className='banner-text'>Making a Difference to Education so that Education can make a Difference to the World.</h2>
+                        <p className='banner-subtext'>Arise! Awake! and Stop not till the goal is reached</p>
+                        </div>
+                    </div>
+                </SwiperSlide>
+            </Swiper>
 
         </div>
 
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,33 +5,37 @@ function Navbar() {
         <div className='w-100'>
 
             <nav className="head w-100">
-                
-                    <div className='header'>
-                        <div className="social-icons">
-                            <ul className='social'>
-                                <li>
-                                    <a href='/'> <i class="fa-brands fa-facebook"></i></a>
-                                </li>
-                                <li>
-                                    <a href='/'> <i class="fa-brands fa-instagram"></i></a>
-                                </li>
-                                <li>
-                                    <a href='/'>  <i class="fa-brands fa-x-twitter"></i></a>
-                                </li>
-                            </ul></div>
-                        <div className='sub-heading'>
-                            <ul className='sub-head'>
-                                <li className='items-sub-head'><a href='/'>About Us</a></li>
-                                <li className='items-sub-head'><a href='/'>Learning Approach</a></li>
-                                <li className='items-sub-head'><a href='/'>Admissions</a></li>
-                                <li className='items-sub-head'><a href='/'>News & Events</a></li>
-                                <li className='items-sub-head'><a href='/'>Contact Us</a></li>
-                                <li className='items-sub-head'><a href='/'>Mandatory Public Disclosure</a></li>
-                            </ul>
 
-                        </div>
+                <div className='header'>
+                    <div className="social-icons">
+                        <ul className='social'>
+                            <li className='highlighted-item'>
+                                <a href='/'><i class="fa-solid fa-phone-volume query"></i>+91 12345 67890</a>
+                            </li>
+
+                            <li>
+                                <a href='/'> <i class="fa-brands fa-facebook"></i></a>
+                            </li>
+                            <li>
+                                <a href='/'> <i class="fa-brands fa-instagram"></i></a>
+                            </li>
+                            <li>
+                                <a href='/'>  <i class="fa-brands fa-x-twitter"></i></a>
+                            </li>
+                        </ul></div>
+                    <div className='sub-heading'>
+                        <ul className='sub-head'>
+                            <li className='items-sub-head'><i class="fa-solid fa-info sub-head-icon"></i><a href='/'>About Us</a></li>
+                            <li className='items-sub-head'><i class="fa-solid fa-book-open sub-head-icon"></i><a href='/'>Learning Approach</a></li>
+                            <li className='items-sub-head'><i class="fa-solid fa-building-columns sub-head-icon"></i><a href='/'>Admissions</a></li>
+                            <li className='items-sub-head'><i class="fa-solid fa-newspaper sub-head-icon"></i><a href='/'>News & Events</a></li>
+                            <li className='items-sub-head'><i class="fa-regular fa-address-card sub-head-icon"></i><a href='/'>Contact Us</a></li>
+                            <li className='items-sub-head'><i class="fa fa-asterisk sub-head-icon"></i><a href='/'>Mandatory Public Disclosure</a></li>
+                        </ul>
+
                     </div>
-              
+                </div>
+
             </nav>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className='container'>
@@ -39,7 +43,7 @@ function Navbar() {
                     <div className='w-100 text-center'>
                         <div className='logo'>
                             <img className='img-logo' src='../assets/logo-5.png' alt=''></img>
-                            {/* <img className='img-logo' src='../assets/logo.jpg' alt=''></img> */}
+                            <button class="admission">Admission 2024-25</button>
                         </div>
                     </div>
                 </div>
